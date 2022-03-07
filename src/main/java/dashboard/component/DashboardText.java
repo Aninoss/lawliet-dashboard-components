7314fa8b8347cb6fa5dd4b2ce83f6a80cdd624a6
Aninoss <leonblech@tutanota.com>
2022-03-07 20:06:13 +0100

    DashboardText styles

--- src/main/java/dashboard/component/DashboardText.java
+++ src/main/java/dashboard/component/DashboardText.java
@@ -7,7 +7,10 @@ public class DashboardText extends DashboardComponent {
 
     public static final int TYPE = 5;
 
+    public enum Style { DEFAULT, ERROR, SUCCESS }
+
     private final String text;
+    private Style style = Style.DEFAULT;
 
     public DashboardText(String text) {
         super(TYPE);
@@ -17,16 +20,27 @@ public class DashboardText extends DashboardComponent {
     public DashboardText(JSONObject json) {
         super(json);
         text = json.getString("text");
+        style = Style.values()[json.getInt("style")];
     }
 
     public String getText() {
         return text;
     }
 
+    public Style getStyle() {
+        return style;
+    }
+
+    public DashboardText setStyle(Style style) {
+        this.style = style;
+        return this;
+    }
+
     @Override
     public JSONObject toJSON() {
         JSONObject json = super.toJSON();
         json.put("text", text);
+        json.put("style", style.ordinal());
         return json;
     }
 
